5f7c4fe83a5a9e3c2aa203eb7497a1b2be6d1221
Ian Stewart <ian.alan.stewart@gmail.com>
2013-11-18 21:40:36 -0800

    create process.cpp, process.h

--- process.cpp
+++ process.cpp
@@ -3,13 +3,40 @@
 // process.cpp (file 3 of 9)
 //
 // Erik Schultz, eriks@sfu.ca, 301034882
-// Ian Stewart [info TO-DO]
+// Ian Stewart, iastewar@sfu.ca, 301190316
+// Yixuan Li, yixuanl@sfu.ca, 301191905
 // ------------------------------------------
 
-#include process.h
-
-// default constructor
-process::process()
+#include <stdlib.h>
+#include "Process.h"
+Process::Process()
 {
+    counter = 0;
+    length = rand()%246+10;
+    instructions = new int[length]; //array of random length between 10 and 255.
 
+    //The instructions are IO instructions with a 10% probability.
+    //The IO instructions are uniformly distributed thoughout the program (unrealistic, maybe change this later)
+    for (int i=0; i<length; i++) {
+        if (rand()%100 < 10) {
+            instructions[i] = IO;
+        }
+        else {
+            instructions[i] = CPU;
+        }
+    }
+}
+
+Process::~Process()
+{
+    delete [] instructions;
+}
+
+int Process::next()
+{
+    if (counter==length) {
+        return END_OF_FILE;
+    } else {
+        return instructions[counter++];
+    }
 }
--- process.h
+++ process.h
@@ -3,19 +3,30 @@
 // process.h (file 2 of 9)
 //
 // Erik Schultz, eriks@sfu.ca, 301034882
-// Ian Stewart [info TO-DO]
-// Yixuan [info TO-DO]
+// Ian Stewart, iastewar@sfu.ca, 301190316
+// Yixuan Li, yixuanl@sfu.ca, 301191905
 // ------------------------------------------
 
 #ifndef PROCESS_H
 #define PROCESS_H
 
-class process 
+// Simulates a process with an array of instructions, which are either
+// CPU-bound computation or else traps to input/output devices.
+
+class Process
 {
-private:
-// private class implementation TO-DO
-public:
-// public class implementation TO-DO
+    public:
+        static const int CPU = 0;
+        static const int IO = 1;
+        static const int END_OF_FILE = -1;
+        int next(void);
+        Process();
+        virtual ~Process();
+    protected:
+    private:
+        int * instructions;
+        int counter;
+        int length;
 };
 
-#endif
+#endif // PROCESS_H
