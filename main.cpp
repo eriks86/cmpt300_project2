7dec61d1d77ebf06e8505fa007c2ba2fda32961e
Ian Stewart <ian.alan.stewart@gmail.com>
2013-11-18 21:12:55 -0800

    update email & id

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 // main.cpp (file 1 of 9)
 //
 // Erik Schultz, eriks@sfu.ca, 301034882
-// Ian Stewart [info TO-DO]
+// Ian Stewart, iastewar@sfu.ca, 301190316
 // Yixuan Li, yixuanl@sfu.ca, 301191905
 // ------------------------------------------
 
