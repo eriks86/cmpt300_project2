ef5d1592cb3787475e9235c02e2855ec1c28d91f
Ian Stewart <ian.alan.stewart@gmail.com>
2013-11-19 23:24:44 -0800

    comment my code like a good boy

--- readyqueue.cpp
+++ readyqueue.cpp
@@ -1,6 +1,6 @@
 // ------------------------------------------
 // CMPT 300 Project 2
-// process.cpp (file 3 of 9)
+// readyqueue.cpp (file 3 of 9)
 //
 // Erik Schultz, eriks@sfu.ca, 301034882
 // Ian Stewart, iastewar@sfu.ca, 301190316
@@ -23,6 +23,8 @@ int readyqueue::getSize() {
 
 //push at the tail
 void readyqueue::push(process * p) {
+	//the level of a process in the multi-level feedback queue 
+	//corresponds to the number of times it has timed out.
 	int level = (p->numTimeouts>=2 ? 2 : p->numTimeouts);
 	node * myNode = new node;
 	myNode->p = p;
--- readyqueue.h
+++ readyqueue.h
@@ -1,6 +1,6 @@
 // ------------------------------------------
 // CMPT 300 Project 2
-// process.h (file 2 of 9)
+// readyqueue.h (file 2 of 9)
 //
 // Erik Schultz, eriks@sfu.ca, 301034882
 // Ian Stewart, iastewar@sfu.ca, 301190316
@@ -12,16 +12,19 @@
 #include "process.h"
 #include <stdlib.h>
 
+//This is a multi-level queue of processes implemented as a linked list.
 class readyqueue 
 {
 private:
-	int size;
-	struct node {
+	int size; //tells the number of processes waiting
+	struct node { //a node in the list, as seen in CMPT 225
 		process * p;
 		node * previous;
 		node * next;
 	};
-	node * queueHead[3];
+	//There are three sub-queues, each with a head and a tail. 0 is the top one. 
+	//When a process times out from the CPU, it goes down a level.
+	node * queueHead[3]; 
 	node * queueTail[3];
 public:
 	readyqueue();
