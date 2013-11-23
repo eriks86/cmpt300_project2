da615837f2cd56dc83d16890ef6cecd30dafd231
Ian Stewart <ian.alan.stewart@gmail.com>
2013-11-22 22:34:50 -0800

    comments

--- schedulers.cpp
+++ schedulers.cpp
@@ -1,8 +1,7 @@
 #include "process.h"
 #include "readyqueue.h"
 #include "blockedqueue.h"
-#include "tests.h"
-//#include "simulationCPU.h"
+//#include "tests.h"
 #include "schedulers.h"
 #include <iostream>
 #include <unistd.h>
@@ -14,10 +13,10 @@ blockedqueue b;
 pthread_t CPUthreads [3];
 pthread_t schedulerThreads [3];
 
-
 void longTermScheduler() {
 	while (true) {
 		if (r.size()>=16) {
+			//we don't want too many processes in the ready queue
 			pthread_yield();
 			usleep(500);
 			continue;
@@ -30,38 +29,48 @@ void longTermScheduler() {
 	}
 }
 
+//this just gets some stuff rolling.
 void * shortTermInitialize(void * arg) {
 	for (int i=0; i<3; i++) {
 		pthread_create(&CPUthreads[i], NULL, CPURunProcess, (void *)r.pop());
+		//run 3 CPUs
 		pthread_create(&schedulerThreads[i], NULL, shortTermScheduler, (void *)&i);
+		//create a STS for each CPU
 	}
 }
 
+//we want to continually check whether each thread is done or not
 void * shortTermScheduler (void * arg) {
 	int i = *(int *)arg;
 	while (true) {
-		pthread_join(CPUthreads[i], NULL);
+		pthread_join(CPUthreads[i], NULL); //try to join the thread
 		pthread_create(&CPUthreads[i], NULL, CPURunProcess, (void *)r.pop());
+		//when the thread is done, make it again
 		pthread_yield();
 	}
 }
 
+//this function simulates the running of a CPU. It goes through the process's instructions.
 void * CPURunProcess (void * arg) {
 	process p = *(process *)arg; 
 	int counter = 0;
 	int next = p.next();
 	while (next!=process::END_OF_FILE) {
-		if (next==process::IO) {
+		if (next==process::IO) { 
+			//this simulates a trap to IO. We want to block the process and resume it later
 			b.Block(&p);
 			return 0;
 		}
 		counter++;
 		if (counter==TIME_QUANTUM) {
+			//this simulates a timing out of the process. We want to add it back to the ready queue.
 			p.numTimeouts++;
 			r.push(&p);
 			return 0;
 		}
 	}
-	//delete &p; //this is when the process has terminated
+	//we reach this point in the code if the process has reached the end of its file
+	//delete &p;
+	//gives an error
 	return 0;
 }
